0f212122df83c6370d130f091c4950b40b000f38
BaoquanDuan <bqduan0626@gmail.com>
2025-10-27 18:41:38 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,47 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-
-# -- Project information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'CHESS'
-copyright = '2026, Duan'
-author = 'd'
-
+author = 'Duan'
 release = '1.0.0'
 version = '0.1.0'
 
-# -- General configuration
-
-python rmd_to_md_rtd.py
-
 extensions = [
-    'myst_parser',       # 支持 .md 文件
+    'myst_parser',         # 支持 Markdown
     'sphinx_rtd_theme',
 ]
 
-# myst-parser 配置，支持 R 代码块
-myst_enable_extensions = [
-    "colon_fence",
-    "deflist",
-    "dollarmath",
-    "html_admonition",
-    "html_image",
-]
-myst_code_languages = ["python", "r", "bash", "c", "cpp", "java"]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
-
-# -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+source_suffix = ['.rst', '.md']
